a735911bab895436fcd9af88747d535a707ae91b
veevaanets <veevaanets@gmail.com>
2026-06-29 15:18:55 +0530

    fix: add control model imports, exports, and associations to barrel (RAN-71)
    Co-authored-by: multica-agent <github@multica.ai>

--- src/lib/models/index.ts
+++ src/lib/models/index.ts
@@ -14,9 +14,23 @@ import { OrganizationModuleConfig } from './OrganizationModuleConfig'
 import { OrganizationFrameworkSelection } from './OrganizationFrameworkSelection'
 import { OrganizationIntegrationIntent } from './OrganizationIntegrationIntent'
 import { WizardState } from './WizardState'
+import { Control } from './Control'
+import { ControlVersion } from './ControlVersion'
+import { ControlImplementationStep } from './ControlImplementationStep'
+import { ControlStepCategory } from './ControlStepCategory'
+import { ControlEvidenceType } from './ControlEvidenceType'
 
 InternalUser.belongsTo(InternalRole, { foreignKey: 'internal_role_id', as: 'role' })
 InternalRole.hasMany(InternalUser, { foreignKey: 'internal_role_id', as: 'users' })
+
+Control.hasMany(ControlVersion, { foreignKey: 'control_id', as: 'versions' })
+ControlVersion.belongsTo(Control, { foreignKey: 'control_id', as: 'control' })
+ControlVersion.hasMany(ControlImplementationStep, { foreignKey: 'control_version_id', as: 'implementationSteps' })
+ControlImplementationStep.belongsTo(ControlVersion, { foreignKey: 'control_version_id', as: 'version' })
+ControlVersion.hasMany(ControlEvidenceType, { foreignKey: 'control_version_id', as: 'evidenceTypes' })
+ControlEvidenceType.belongsTo(ControlVersion, { foreignKey: 'control_version_id', as: 'version' })
+ControlImplementationStep.belongsTo(ControlStepCategory, { foreignKey: 'category_id', as: 'category' })
+ControlStepCategory.hasMany(ControlImplementationStep, { foreignKey: 'category_id', as: 'steps' })
 
 export {
   InternalUser,
@@ -34,6 +48,11 @@ export {
   OrganizationFrameworkSelection,
   OrganizationIntegrationIntent,
   WizardState,
+  Control,
+  ControlVersion,
+  ControlImplementationStep,
+  ControlStepCategory,
+  ControlEvidenceType,
 }
 
 export function initModels(): void {
@@ -56,4 +75,9 @@ export function initModels(): void {
   OrganizationFrameworkSelection
   OrganizationIntegrationIntent
   WizardState
+  Control
+  ControlVersion
+  ControlImplementationStep
+  ControlStepCategory
+  ControlEvidenceType
 }
